4d3ee4011eb0ecf701cdb99c652873118d3a690c
Revilo1882 <oliverrharris@gmail.com>
2018-09-07 11:52:29 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Apple iPhone 8
+      </div>
+    );
+  }
+}
+
+export default App;
