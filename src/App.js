096eb5b369d11b36601f7e309355dcb0275b7d00
Revilo1882 <oliverrharris@gmail.com>
2018-09-07 13:15:39 +0100

    Add linter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import data from './data/phones.json'
+import data from './data/phones.json';
 
 
 class App extends Component {
-  render() {
-    console.log(data[0].deviceSummary)
-    return (
-      <div>
-        <h1>{data[0].groupName}</h1>
-        <h4>{data[0].deviceSummary[0].displayDescription}</h4>
-      </div>
-    );
-  }
+	render() {
+		console.log('devise list',data[0].deviceSummary);
+		return (
+			<div>
+				<h1>{data[0].groupName}</h1>
+				<h4>{data[0].deviceSummary[0].displayDescription}</h4>
+			</div>
+		);
+	}
 }
 
 export default App;
