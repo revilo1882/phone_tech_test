2502094f1017a04e3b4159470c3371d7fd545c60
Revilo1882 <oliverrharris@gmail.com>
2018-09-07 14:18:17 +0100

    Add basic styling and selections

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ class App extends Component {
 		return (
 			<div>
 				<h1>{data[0].groupName}</h1>
-				<h4>{data[0].deviceSummary[0].displayDescription}</h4>
+				<p>{data[0].deviceSummary[0].displayDescription}</p>
+				<div className='colour'>Colour:<div className='selected'>Gold</div></div>
+				<div className='capacity'>Capacity:<div className='selected'>64GB</div></div>
+        <br />
+				<span className='gold'></span>
+				<span className='silver'></span>
+				<span className='spaceGrey'></span>
+				<span className='size'>256</span>
+				<span className='size'>64</span>
 			</div>
 		);
 	}
