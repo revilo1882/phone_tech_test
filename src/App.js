b61cf3f683ab09773ce68e9bc03a8d928758f152
Revilo1882 <oliverrharris@gmail.com>
2018-09-07 16:21:37 +0100

    Add image and change based on state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import data from './data/phones.json';
-
+import Gold from './images/Apple_iPhone_8_Gold-full-product-front.png';
+import Silver from './images/Apple_iPhone_8_Silver_WS2-full-product-front.png';
+import SpaceGrey from './images/Apple_iPhone_8_Space_Grey_WS2-full-product-front.png';
 
 class App extends Component {
 	constructor() {
@@ -8,51 +10,72 @@ class App extends Component {
 
 		this.state = {
 			colour: 'Gold',
-			capacity: '64GB'
+			capacity: '64GB',
+			image: Gold
 		};
 	}
 
 	render() {
-		console.log('device list',data[0].deviceSummary);
 		return (
 			<div>
-				<h1>{data[0].groupName}</h1>
-				<p>{data[0].deviceSummary[0].displayDescription}</p>
-				<div className='colour'>
-          Colour:
-					<div className='selected'>
-						{this.state.colour}
-					</div>
+				<div>
+					<img
+						src={this.state.image}
+						alt=''
+						style={{
+							float: 'left',
+							width: '25%'}} />
 				</div>
-				<div className='capacity'>
-          Capacity:
-					<div className='selected'>
-						{this.state.capacity}
+				<div>
+					<h1>{data[0].groupName}</h1>
+					<p>{data[0].deviceSummary[0].displayDescription}</p>
+				</div>
+				<div>
+					<div className='colour'>
+            Colour:
+						<div className='selected'>
+							{this.state.colour}
+						</div>
 					</div>
+					<div className='capacity'>
+            Capacity:
+						<div className='selected'>
+							{this.state.capacity}
+						</div>
+					</div>
+					<br />
+					<span
+						className='gold'
+						onClick={() => this.setState({ colour: 'Gold', image: Gold })}>
+					</span>
+					<span
+						className='silver'
+						onClick={() => this.setState({ colour: 'Silver', image: Silver })}>
+					</span>
+					<span
+						className='spaceGrey'
+						onClick={() => this.setState({ colour: 'Space Grey', image: SpaceGrey })}>
+					</span>
+					<span
+						className='size'
+						onClick={() => this.setState({ capacity: '256GB' })}>
+            256
+					</span>
+					<span
+						className='size'
+						onClick={() => this.setState({ capacity: '64GB' })}>
+            64
+					</span>
 				</div>
 				<br />
-				<span
-					className='gold'
-					onClick={() => this.setState({ colour: 'Gold' })}>
-				</span>
-				<span
-					className='silver'
-					onClick={() => this.setState({ colour: 'Silver' })}>
-				</span>
-				<span
-					className='spaceGrey'
-					onClick={() => this.setState({ colour: 'Space Grey' })}>
-				</span>
-				<span
-					className='size'
-					onClick={() => this.setState({ capacity: '256GB' })}>
-          256
-				</span>
-				<span
-					className='size' 
-					onClick={() => this.setState({ capacity: '64GB' })}>
-          64
-				</span>
+				<br />
+				<br />
+				<br />
+				<div className='cost'>
+					<span>from £1149 upfront cost</span>
+					{' | '}
+					<span>when you pay £43.20 a month</span>
+				</div>
 			</div>
 		);
 	}
