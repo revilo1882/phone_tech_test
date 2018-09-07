77986f114ef9dc2c72f34a14d28558f2966435d2
Revilo1882 <oliverrharris@gmail.com>
2018-09-07 14:49:00 +0100

    Add initial state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import data from './data/phones.json';
 
 
 class App extends Component {
+  constructor() {
+		super();
+
+		this.state = {
+			colour: 'Gold',
+			capacity: '64GB'
+		};
+	}
+
 	render() {
-		console.log('devise list',data[0].deviceSummary);
+		console.log('device list',data[0].deviceSummary);
 		return (
 			<div>
 				<h1>{data[0].groupName}</h1>
 				<p>{data[0].deviceSummary[0].displayDescription}</p>
-				<div className='colour'>Colour:<div className='selected'>Gold</div></div>
-				<div className='capacity'>Capacity:<div className='selected'>64GB</div></div>
-        <br />
+				<div className='colour'>Colour:<div className='selected'>{this.state.colour}</div></div>
+				<div className='capacity'>Capacity:<div className='selected'>{this.state.capacity}</div></div>
+				<br />
 				<span className='gold'></span>
 				<span className='silver'></span>
 				<span className='spaceGrey'></span>
