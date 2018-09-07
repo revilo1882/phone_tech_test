363fb5b4877d7630737961c7db26eb54103bfb60
Revilo1882 <oliverrharris@gmail.com>
2018-09-07 13:10:59 +0100

    Link data to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import data from './data/phones.json'
 
 
 class App extends Component {
   render() {
+    console.log(data[0].deviceSummary)
     return (
       <div>
-        Apple iPhone 8
+        <h1>{data[0].groupName}</h1>
+        <h4>{data[0].deviceSummary[0].displayDescription}</h4>
       </div>
     );
   }
