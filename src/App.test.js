900c9eef6e26915a360ffb7d26a0c5d30df6c19e
Revilo1882 <oliverrharris@gmail.com>
2018-09-08 17:07:49 +0100

    Add test for image change when silver is clicked

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { configure, mount } from 'enzyme';
 import App from './App';
 import Gold from './images/Apple_iPhone_8_Gold-full-product-front.png';
+import Silver from './images/Apple_iPhone_8_Silver_WS2-full-product-front.png';
+import SpaceGrey from './images/Apple_iPhone_8_Space_Grey_WS2-full-product-front.png';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
@@ -17,21 +19,25 @@ describe('App', () => {
 		expect(app.state().colour).toEqual('Gold');
 	});
 
-	it('sets the initial colour state', () => {
+	it('sets the initial capacity state', () => {
 		expect(app.state().capacity).toEqual('64GB');
 	});
 
-	it('sets the initial colour state', () => {
+	it('sets the initial image state', () => {
 		expect(app.state().image).toEqual(Gold);
-	})
+	});
 
 	describe('changes the state on click of the Silver div', () =>{
 		beforeEach(() => {
 			app.find('.silver').simulate('click');
 		});
 
-		it('sets the initial colour state', () => {
+		it('changes the colour state when `silver` is clicked', () => {
 			expect(app.state().colour).toEqual('Silver');
+		});
+
+		it('changes the image state when `silver` is clicked', () => {
+			expect(app.state().image).toEqual(Silver);
 		});
 
 	});
