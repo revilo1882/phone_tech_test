c47b38e3de4158bc07785577fc43075fa8161420
Revilo1882 <oliverrharris@gmail.com>
2018-09-08 18:23:22 +0100

    Clean App.test file

--- src/App.test.js
+++ src/App.test.js
@@ -11,97 +11,117 @@ configure({ adapter: new Adapter() });
 describe('App', () => {
 	let app = mount(<App />);
 
-	it('renders the App title', () => {
-		expect(app.find('h1').text()).toEqual('Apple iPhone 8');
-	});
-
-	it('sets the initial colour state', () => {
-		expect(app.state().colour).toEqual('Gold');
-	});
-
-	it('sets the initial capacity state', () => {
-		expect(app.state().capacity).toEqual('64GB');
-	});
-
-	it('sets the initial image state', () => {
-		expect(app.state().image).toEqual(Gold);
-	});
-
-	it('sets the style of the Gold element', () => {
-		expect(app.find('.gold').prop('style')).toEqual({boxShadow: '0 0 0 1pt SeaGreen'});
-	});
-
-  it('sets the style of the Silver element', () => {
-    expect(app.find('.silver').prop('style')).toEqual({boxShadow: 'none'});
-  });
-
-  it('sets the style of the Space Grey element', () => {
-    expect(app.find('.spaceGrey').prop('style')).toEqual({boxShadow: 'none'});
-  });
-
-  it('sets the style of the 64GB element', () => {
-    expect(app.find('.sixtyFour').prop('style')).toEqual({boxShadow: '0 0 0 1pt SeaGreen'});
-  });
-
-  it('sets the style of the 256GBelement', () => {
-    expect(app.find('.twoFiveSix').prop('style')).toEqual({boxShadow: 'none'});
-  });
-
-	describe('changes the state on click of the Silver div', () =>{
-		beforeEach(() => {
-			app.find('.silver').simulate('click');
-		});
-
-		it('changes the colour state when `silver` is clicked', () => {
-			expect(app.state().colour).toEqual('Silver');
-		});
-
-		it('changes the image state when `silver` is clicked', () => {
-			expect(app.state().image).toEqual(Silver);
+	describe('initial rendering', () => {
+		it('renders the App title', () => {
+			expect(app.find('h1').text()).toEqual('Apple iPhone 8');
 		});
 	});
 
-	describe('changes the state on click of the SpaceGrey div', () =>{
-		beforeEach(() => {
-			app.find('.spaceGrey').simulate('click');
-		});
-
-		it('changes the colour state when `spaceGrey` is clicked', () => {
-			expect(app.state().colour).toEqual('Space Grey');
-		});
-
-		it('changes the image state when `spaceGrey` is clicked', () => {
-			expect(app.state().image).toEqual(SpaceGrey);
-		});
-	});
-
-	describe('changes the state back on click of the Gold div', () =>{
-		beforeEach(() => {
-			app.find('.spaceGrey').simulate('click');
-			app.find('.gold').simulate('click');
-		});
-
-		it('changes the colour state when `spaceGrey` is clicked', () => {
+	describe('initial state', () => {
+		it('sets the initial colour state', () => {
 			expect(app.state().colour).toEqual('Gold');
 		});
 
-		it('changes the image state when `spaceGrey` is clicked', () => {
+		it('sets the initial capacity state', () => {
+			expect(app.state().capacity).toEqual('64GB');
+		});
+
+		it('sets the initial image state', () => {
 			expect(app.state().image).toEqual(Gold);
 		});
 	});
 
-	describe('changes the state on click of the twoFiveSix div', () =>{
+	describe('initial styling', () => {
+		it('sets the style of the Gold element', () => {
+			expect(app.find('.gold').prop('style')).toEqual({boxShadow: '0 0 0 1pt SeaGreen'});
+		});
+
+		it('sets the style of the Silver element', () => {
+			expect(app.find('.silver').prop('style')).toEqual({boxShadow: 'none'});
+		});
+
+		it('sets the style of the Space Grey element', () => {
+			expect(app.find('.spaceGrey').prop('style')).toEqual({boxShadow: 'none'});
+		});
+
+		it('sets the style of the 64GB element', () => {
+			expect(app.find('.sixtyFour').prop('style')).toEqual({boxShadow: '0 0 0 1pt SeaGreen'});
+		});
+
+		it('sets the style of the 256GBelement', () => {
+			expect(app.find('.twoFiveSix').prop('style')).toEqual({boxShadow: 'none'});
+		});
+	});
+
+	describe('clicking on the Silver element', () =>{
+		beforeEach(() => {
+			app.find('.silver').simulate('click');
+		});
+
+		describe('changes the state to Silver', () => {
+			it('changes the colour state when `silver` is clicked', () => {
+				expect(app.state().colour).toEqual('Silver');
+			});
+
+			it('changes the image state when `silver` is clicked', () => {
+				expect(app.state().image).toEqual(Silver);
+			});
+		});
+
+		describe('clicking back on the Gold element', () =>{
+			beforeEach(() => {
+				app.find('.gold').simulate('click');
+			});
+
+			describe('changes the state to Gold', () => {
+				it('changes the colour state when `gold` is clicked', () => {
+					expect(app.state().colour).toEqual('Gold');
+				});
+
+				it('changes the image state when `gold` is clicked', () => {
+					expect(app.state().image).toEqual(Gold);
+				});
+			});
+		});
+	});
+
+	describe('clicking on the SpaceGrey element', () =>{
+		beforeEach(() => {
+			app.find('.spaceGrey').simulate('click');
+		});
+
+		describe('changes the state to Space Grey', () => {
+			it('changes the colour state when `spaceGrey` is clicked', () => {
+				expect(app.state().colour).toEqual('Space Grey');
+			});
+
+			it('changes the image state when `spaceGrey` is clicked', () => {
+				expect(app.state().image).toEqual(SpaceGrey);
+			});
+		});
+	});
+
+	describe('clicking on the twoFiveSix element', () =>{
 		beforeEach(() => {
 			app.find('.twoFiveSix').simulate('click');
 		});
 
-		it('changes the capacity state when `twoFiveSix` is clicked', () => {
-			expect(app.state().capacity).toEqual('256GB');
+		describe('changes the state when `256GB` is clicked', () => {
+			it('changes the capacity state when `twoFiveSix` is clicked', () => {
+				expect(app.state().capacity).toEqual('256GB');
+			});
 		});
 
-		it('changes the capacity state back when `sixtyFour` is clicked', () => {
-			app.find('.sixtyFour').simulate('click');
-			expect(app.state().capacity).toEqual('64GB');
+		describe('clicking on the sixtyFour element', () =>{
+			beforeEach(() => {
+				app.find('.sixtyFour').simulate('click');
+			});
+
+			describe('changes the state when `64GB` is clicked', () => {
+				it('changes the capacity state back when `sixtyFour` is clicked', () => {
+					expect(app.state().capacity).toEqual('64GB');
+				});
+			});
 		});
 	});
 });
